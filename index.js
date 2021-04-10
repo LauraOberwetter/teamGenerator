38e5a7a54eb706a168fc5b96f32a8d6a46d2917a
Laura Oberwetter <leoberwetter@gmail.com>
2021-04-10 08:42:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer")
+
+const fs = require("fs") // writing to HTML with fs method //what to write // where to write
+
+const path = require("path");//path module provides utilities for working with file and directory paths
+
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager")
+
+
+
