0d8c0668b237b9f605d5952fb9c9a792b670e36d
Laura Oberwetter <leoberwetter@gmail.com>
2021-04-11 19:33:39 -0500

    initiated inquirer

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const inquirer = require("inquirer")
 
 const fs = require("fs") // writing to HTML with fs method //what to write // where to write
-
 const path = require("path");//path module provides utilities for working with file and directory paths
 
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager")
+const Manager = require("./lib/Manager");
+const Employee = require("./lib/Employee");
 
 
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Employee name:',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Employee ID:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Employee email address:',
+    },
+    {
+        type: 'checkbox',
+        name: 'role',
+        message: 'Employee role:',
+        choices: [
+            'Manager', 'Engineer', 'Intern'
+        ],
+    },
 
+]
