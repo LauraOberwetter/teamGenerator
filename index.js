510dffcf348ad47c19d46b7e2adef0a60221bb5a
Laura Oberwetter <leoberwetter@gmail.com>
2021-04-11 21:08:50 -0500

    path issues resolved

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const inquirer = require("inquirer")
 const fs = require("fs") // writing to HTML with fs method //what to write // where to write
 const path = require("path");//path module provides utilities for working with file and directory paths
 
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
-const Employee = require("./lib/Employee");
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Employee = require('./lib/Employee');
 
 
 const questions = [
